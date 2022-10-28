07293a47512c2ed83474edc4318dfa372d59de6a
ChetanKamble1 <chetankamble189@gmail.com>
2022-10-28 19:37:30 +0530

    code

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
 
   const User = sequelize.define("users", {
-    candidate_id: {
+    uuid: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,

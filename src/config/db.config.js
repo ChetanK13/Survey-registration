b403d8b1807a62f66f1b40a80a147f9299171b42
ChetanKamble1 <chetankamble189@gmail.com>
2022-11-10 12:07:27 +0530

    10/11/2022

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,17 +1,17 @@
 /** @format */
 
-// module.exports={
-//     HOST:'localhost',
-//     USER:'root',
-//     PASSWORD :'Qwerty!12',
-//     DB:'survey',
-//    dialect:'mysql'
-// }
+module.exports={
+    HOST:'localhost',
+    USER:'root',
+    PASSWORD :'Qwerty!12',
+    DB:'survey',
+   dialect:'mysql'
+}
 
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "Chetan@123",
-  DB: "survey",
-  dialect: "mysql",
-};
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "Chetan@123",
+//   DB: "survey",
+//   dialect: "mysql",
+// };
